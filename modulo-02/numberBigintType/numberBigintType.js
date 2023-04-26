6b0da7260224bebbe5b49be0cf47e34aace8e623
InfoDevEstudent <infodev.infodev@gmail.com>
2023-04-26 12:34:08 -0300

    Adicionando Bigint

--- modulo-02/numberBigintType/numberBigintType.js
+++ modulo-02/numberBigintType/numberBigintType.js
@@ -8,3 +8,12 @@ console.log('Number - Ponto Flutuante ...: ', num1);
 console.log('Number - Hexadecimal ...: ', num2);
 console.log('Number - Octal ...: ', num3);
 console.log('Number - Binário ...: ', num4);
+// Exemplo: bigint
+let big1 = 9007199254740991n;
+let big2 = 9007199254099n;
+let big3 = 0xffffffffffffffn;
+let big4 = 1801439850948198n;
+console.log('Bigint ...: ', big1);
+console.log('Bigint ...: ', big2);
+console.log('Bigint ...: ', big3);
+console.log('Bigint ...: ', big4);
--- modulo-02/numberBigintType/numberBigintType.ts
+++ modulo-02/numberBigintType/numberBigintType.ts
@@ -9,3 +9,15 @@ console.log('Number - Ponto Flutuante ...: ', num1);
 console.log('Number - Hexadecimal ...: ', num2);
 console.log('Number - Octal ...: ', num3);
 console.log('Number - Binário ...: ', num4);
+
+// Exemplo: bigint
+
+let big1: bigint = 9007199254740991n;
+let big2: bigint = 9007199254099n;
+let big3: bigint = 0xffffffffffffffn;
+let big4: bigint = 1801439850948198n;
+
+console.log('Bigint ...: ', big1);
+console.log('Bigint ...: ', big2);
+console.log('Bigint ...: ', big3);
+console.log('Bigint ...: ', big4);
