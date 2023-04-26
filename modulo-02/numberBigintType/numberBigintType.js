93e3b9dc9a948f8116a66ca230b8a4c49cef3ee5
InfoDevEstudent <infodev.infodev@gmail.com>
2023-04-26 11:55:25 -0300

    Adicionando Number e Bigint

---
+++ modulo-02/numberBigintType/numberBigintType.js
@@ -0,0 +1,10 @@
+"use strict";
+// Exemplos: number
+let num1 = 23.0;
+let num2 = 0X78CF;
+let num3 = 0O557;
+let num4 = 0B11001;
+console.log('Number - Ponto Flutuante ...: ', num1);
+console.log('Number - Hexadecimal ...: ', num2);
+console.log('Number - Octal ...: ', num3);
+console.log('Number - Binário ...: ', num4);
