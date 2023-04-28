48cfb880bac9e43506e5abb400949818557bb3b1
infodevestudent <infodev.infodev@gmail.com>
2023-04-28 18:08:01 -0300

    Adicionando stringType

--- modulo-02/04stringType/stringType.js
+++ modulo-02/04stringType/stringType.js
@@ -1,1 +1,11 @@
 "use strict";
+// Exemplo 01 Single Quotes
+let nomeCompleto = 'Sérgio Felipe';
+console.log(nomeCompleto);
+// Exemplo 02 Double Quotes
+let areaDeAtuacao = "Desenvolvedor Full Stack";
+console.log(areaDeAtuacao);
+// Exemplo 03 Back Ticks
+let nomeDaEmpresa = 'Ambev Tech';
+let dadosDoFuncionario = `Seja Bem-Vindo ${nomeCompleto}! Você será nosso novo ${areaDeAtuacao}, na empresa ${nomeDaEmpresa}.`;
+console.log(dadosDoFuncionario);
---
+++ modulo-02/04stringType/stringType.ts
@@ -0,0 +1,13 @@
+// Exemplo 01 Single Quotes
+let nomeCompleto: string = 'Sérgio Felipe';
+console.log(nomeCompleto);
+
+// Exemplo 02 Double Quotes
+let areaDeAtuacao: string = "Desenvolvedor Full Stack";
+console.log(areaDeAtuacao);
+
+// Exemplo 03 Back Ticks
+let nomeDaEmpresa: string = 'Ambev Tech';
+
+let dadosDoFuncionario: string = `Seja Bem-Vindo ${nomeCompleto}! Você será nosso novo ${areaDeAtuacao}, na empresa ${nomeDaEmpresa}.`;
+console.log(dadosDoFuncionario);
